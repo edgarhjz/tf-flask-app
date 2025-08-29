1ebd629fa4d7612d1c59c5462bfcbcc497977883
Edgar Hernandez <edgar.hdjz@gmail.com>
2025-08-28 21:50:25 -0600

    Create the VPC

--- variables.tf
+++ variables.tf
@@ -2,7 +2,6 @@ variable "aws_region" {
   description = "The AWS region to deploy the application"
   type = string
   default     = "us-east-1"
-<<<<<<< HEAD
 }
 
 variable "vpc_cidr_block" {
@@ -10,6 +9,3 @@ variable "vpc_cidr_block" {
   type        = string
   default     = "10.0.0.0/16"
 }
-=======
-}
->>>>>>> 2374d8dbbc433aeb88d8ff54fa79bff318f8205c
