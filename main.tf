f33ead44b8117b654f8855687cd87eb302ee4268
Edgar Hernandez <edgar.hdjz@gmail.com>
2025-09-02 22:30:15 -0600

    Add security group

--- main.tf
+++ main.tf
@@ -46,3 +46,35 @@ resource "aws_route_table_association" "as" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
+
+# Security Group
+resource "aws_security_group" "web_server_sg" {
+  name = "web_server_sg"
+  description = "Allow HTTP and SSH inbound traffic"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "flask-app-sg"
+  }
+}
